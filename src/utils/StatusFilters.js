6a6f3e8c5240c050d1a2b483401be42243355abd
ghada <ghadaemad011@gmail.com>
2023-10-21 23:26:48 +0300

    finish utils files

---
+++ src/utils/StatusFilters.js
@@ -0,0 +1,5 @@
+export const StatusFilters={
+    All:'All',
+    Active:'active',
+    Completed:'completed'
+}
---
+++ src/utils/colors.js
@@ -0,0 +1,2 @@
+export const availableColors = ["green", "blue", "red", "purple", "orange"];
+export const capitalize = (s) => s[0].toUpperCase() + s.slice(1);
