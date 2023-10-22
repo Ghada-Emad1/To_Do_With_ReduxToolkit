7ba960fe98c1f2f0fc8a187bff069127f1b75e6b
ghada <ghadaemad011@gmail.com>
2023-10-23 00:05:56 +0300

    adding components to App 🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import Header from "./components/Header";
+
+import Footer from "./components/Footer";
+
+import TodoList from "./components/TodoList";
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="h-[100vh] mx-auto flex flex-col w-[900px]">
+      <h1 className="font-bold text-4xl text-center m-12 text-Red">
+        To Do App
+      </h1>
+      <div className="flex flex-col gap-6 text-center items-center justify-center">
+        <Header />
+        <TodoList />
+        <Footer />
+      </div>
     </div>
   );
 };
