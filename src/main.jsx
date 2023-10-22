acdd31e201fd823274ac25ccc5e7732765b73b32
ghada <ghadaemad011@gmail.com>
2023-10-23 00:08:41 +0300

    adding some dependencies 😵‍💫

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { ThemeProvider } from "@material-tailwind/react";
+import "./apis/server.js";
+import { fetchTodos } from "./ReducerSlices/todoSlice.js";
+store.dispatch(fetchTodos());
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>,
-)
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'Red':'#D62828',
+        'Dark_Blue':'#003049',
+        'Orange':'#F77F00',
+        'xanthous':'#FCBF49',
+        'Vanilla':'#EAE2B7'
+      }
+    },
   },
   plugins: [],
 }
